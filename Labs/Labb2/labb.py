c465eaf5c1cb6f2513a8675b4ef5868eb8692ed0
JohanNilsenS <johanstjernquist1@gmail.com>
2023-09-16 11:52:12 +0200

    Task completed!

--- Labs/Labb2/labb.py
+++ Labs/Labb2/labb.py
@@ -13,6 +13,7 @@ pikachu = []
 pichu = []
 #Testpoints
 testpoints = [[25,32],[24.2,31.5],[22,34],[20.5,34]]
+datastorage = []
 
 #Data sorter/separator fixes the " 0" and " 1" from import of csv file
 for i in data:
@@ -41,28 +42,46 @@ for i in pikachu:
 
 #Draws the plot
 plt.show()
+#NEW TRY!!
 
-def comparator(testpoints,pichu,pikachu):
-  for i in testpoints:
-    print(i)
-    x1 = i[0]
-    y1 = i[1]
-    for index in pichu:
-      x2 = index[0]
-      y2 = index[1]
+for test in testpoints:
+  print(f"{test} point")
+  x1 = test[0]
+  y1 = test[1]
+  
+
+#Old try
+#def comparator(testpoints,db1,db2):
+#  for i in testpoints:
+#    print(i)
+#    x1 = float(i[0])
+#    y1 = float(i[1])
+#    for index in db1:
+#      x2 = float(index[0])
+#      y2 = float(index[1])
+#      compare(x1,x2,y1,y2)
+#    for index in db2:
+#      x2 = float(index[0])
+#      y2 = float(index[1])
+#      compare(x1,x2,y1,y2)
+
+#def compare(x1,x2,y1,y2):
+#  distance = math.sqrt((x2-x1)**2+(y2-y1)**2)
+#  datastorage.append(distance)
 
 
+#comparator(testpoints[0],pichu,pikachu)
+#print(pikachu[0])
 # d=√((x2 – x1)² + (y2 – y1)²) 
+
 # Vad jag vill göra är att kolla avståndet ifrån testpunkt till alla punkter i pichu och pikachu.
 # Sen tar jag medianen av avståndet emellan pichu och pikachu till testpunkt och assignar label till den medianen som var närmast testpunkten
-x1 = 1
-x2 = 2
 
-y1 = 1
-y2 = 2
 
-distance = math.sqrt((x2-x1)**2+(y2-y1)**2)
+#comparator(testpoints, pikachu)
+#print(len(datastorage))
+#nam = 0
+#for num in datastorage:
+#  nam = num + nam
 
-print(distance)
-
-comparator(testpoints, pichu, pikachu)
+#print(nam / len(datastorage))*/
