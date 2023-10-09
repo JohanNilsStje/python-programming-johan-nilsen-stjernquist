5ed8ad7010d32f0a20c69c42e0862b1de7eb3976
JohanNilsenS <johanstjernquist1@gmail.com>
2023-10-09 10:18:10 +0200

    Startade labb3

---
+++ Labs/Labb3/geometry_shapes.py
@@ -0,0 +1,19 @@
+
+#Circle Class
+class Circle:
+  def __init__(self, x, y, radius):
+    self.x = x
+    self.y = y
+    self.radius = radius
+
+
+
+#Rectangle Class
+class Rectangle:
+  def __init__(self, x, y, side1, side2):
+    self.x_pos = x
+    self.y_pos = y
+    self.side1 = side1
+    self.side2 = side2
+
+#Circumference = π × diameter = 2π × radius
