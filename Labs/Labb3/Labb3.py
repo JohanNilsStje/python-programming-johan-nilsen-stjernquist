5452f3f5603c59a2ff519c262c1bec9c72650e79
JohanNilsenS <johanstjernquist1@gmail.com>
2023-10-17 12:13:09 +0200

    Docstrings and new compare function

--- Labs/Labb3/Labb3.py
+++ Labs/Labb3/Labb3.py
@@ -1,14 +1,61 @@
 from geometry_shapes import *
 
-cirkel1 = Circle(x=0,y=0, radius=1) # enhetscirkel
-cirkel2 = Circle(x=1,y=1, radius=1)
+
+undefined_shape = Geometry_common(x=0,y=0)
+
+cirkel1 = Circle(x=0,y=0, radius=2) # enhetscirkel
+cirkel2 = Circle(x=0,y=0, radius=1)
 rektangel = Rectangle(x=0,y=0,side1=1, side2=1)
 
-print(f"{cirkel1.radius == cirkel2.radius} cirkel1.radius == cirkel2.radius") # Not sure how I sohuld make cirkel1 and cirkel2 return the radius when the object is called
-print(f"{cirkel2==rektangel} cirkel2 == rektangel") # False (works somehow?)
+
+
+print(f"{cirkel1.radius == cirkel2.radius} cirkel1.radius == cirkel2.radius")
+print(f"{cirkel2==rektangel} cirkel2 == rektangel") # False
 print(f"{cirkel1.is_inside(0.5, 0.5)} is_inside(0.5,0.5)") # True 
 cirkel1.translate(5,5)
 print(f"{cirkel1.is_inside(0.5, 0.5)} is_inside(0.5,0.5)") # False
 cirkel1.translate(1.5,5) # ge ValueError med lämplig kommentar
 
+repr(rektangel)
 print(rektangel)
+repr(cirkel1)
+print(cirkel1)
+repr(undefined_shape)
+print(undefined_shape)
+
+print(Geometry_common.__doc__)
+
+
+print(rektangel.is_inside(0.5,0.5))
+rektangel.translate(3,3)
+print(rektangel.is_inside(0.5,0.5))
+
+print(cirkel1.area)
+print(cirkel1.circumfrance)
+
+print(repr(cirkel1))
+print(cirkel1)
+
+print(cirkel1 > cirkel2)
+
+#Chat GPT wrote this testcode:
+circle = Circle(x=0, y=0, radius=3)
+# Test methods and properties for the Circle object
+print("Circle Properties:")
+print("Radius:", circle.radius)
+print("Area:", circle.area)
+print("Circumference:", circle.circumfrance)
+print("Is a unit circle:", circle.is_unit_circle())
+print("Is (2, 2) inside the circle:", circle.is_inside(2, 2))
+
+# Create a Rectangle object
+rectangle = Rectangle(x=0, y=0, side1=4, side2=5)
+
+# Test methods and properties for the Rectangle object
+print("\nRectangle Properties:")
+print("Side 1:", rectangle.side1)
+print("Side 2:", rectangle.side2)
+print("Area:", rectangle.area)
+print("Circumference:", rectangle.circumfrance)
+print("Is a square:", rectangle.is_square())
+print("Is (2, 2) inside the rectangle:", rectangle.is_inside(2, 2))
--- Labs/Labb3/geometry_shapes.py
+++ Labs/Labb3/geometry_shapes.py
@@ -1,19 +1,39 @@
 import math
 class Geometry_common:
+  """
+  Parent Class.
+  This class is used to build simple geometry shapes.
+  Contains fuctions and properties such as
+  get_x, set_x, get_y, set_y, translate(x,y) 
+  comparators for <= < == > >= (Checks if compared object contains atr radius and area)
+  It also contains property for area and circumfrance
+  """
   def __init__(self,x,y):
     #Positioner i Koordinatsystem
     self._x = int(x)
     self._y = int(y)
 
+  @property
+  def area(self):
+    return None
+  @property
+  def circumfrance(self):
+    return None
+
   def get_x(self):
+    """Returns the x value of the object"""
     return self._x
   def get_y(self):
+    """Returns the y value of the object"""
     return self._y
   def set_x(self, value):
+    """Sets the x value of the object"""
     self._x = value
   def set_y(self, value):
+    """Sets the y value of the object"""
     self._y = value
   def translate(self,x,y):
+    """Sets the x and y value of the object"""
     try:
       self._x = int(x)
       self._y = int(y)
@@ -21,67 +41,124 @@ class Geometry_common:
       print("You need to enter int or float. Example translate(int,int), translate(float,float) if you do translate(str, str) this message will apear!")
     pass
 
+  #Comparators magic methods ### The compare method was  taken from ChatGPT before I had written all the code in blocks but ChatGPT reduced the code
+  def _compare_attributes(self, other, comparison_operator):
+    if hasattr(other, "radius") and hasattr(self, "radius"):
+      return comparison_operator(self.radius, other.radius)
+    elif hasattr(other, "area") and hasattr(self, "area"):
+      return comparison_operator(self.area, other.area)
+    else:
+      raise ValueError("Can't compare")
+
+  def __eq__(self, other):
+    return self._compare_attributes(other, lambda a, b: a == b)
+
+  def __lt__(self, other):
+    return self._compare_attributes(other, lambda a, b: a < b)
+
+  def __gt__(self, other):
+    return self._compare_attributes(other, lambda a, b: a > b)
+
+  def __le__(self, other):
+    return self._compare_attributes(other, lambda a, b: a <= b)
+
+  def __ge__(self, other):
+    return self._compare_attributes(other, lambda a, b: a >= b)
+  
+  def __str__(self) -> str:
+    return f'Super Class that is used to build Circles and Rectangles'
+  def __repr__(self):
+    return f"When building a geometry shape it is used as: class ShapeName(Geometry_common):"
+
 ##################
 class Circle(Geometry_common):
+  """The Circle class.
+  Is used to build a circle object with custom fuctions such as: set_radius(radius), is_unit_circle(), is_inside(input_x,input_y)
+  it also contains property for area and circumfrance
+  """
   def __init__(self, x, y, radius):
+    """Initialize a Circle object with the given coordinates and radius."""
     super().__init__(x,y)
     self.radius = radius
-  
-
-  #Area / Circumfrance for Circle
+     
   def set_radius(self, value):
+    """Set the radius of the circle."""
     self.radius = value
-
-  def get_area(self):
-    area = 3.1415 * (self.radius*self.radius)
-    print(f"Area = {area}")
-    return area
-  
-  def get_circumfrence(self):
-    # C = 2πr
-    circumfrance = 3.14 * 2 * self.radius
-    print(f"Circumfrance = {circumfrance}")
-    return circumfrance
+  #Area / Circumfrance for Circle
+  @property
+  def area(self):
+    """Area property for the Circle class"""
+    return math.pi * (self.radius*self.radius)
+  @property
+  def circumfrance(self):
+    """Circumfrance property for the Circle class"""
+    return math.pi * 2 * self.radius
   
   def is_unit_circle(self):
+    """Checks if Circle object is a unit circle. A unit circle is a circle with radius of 1"""
     return self.radius == 1
   
   def is_inside(self,input_x,input_y):
+    """Checks if the input coordinates is within the Circles radius"""
     return math.dist([self.get_x(),self.get_y()],[input_x,input_y]) < self.radius
-  #Magic Method
-  def __eq__(self, other):
-    try:
-      return self.radius == other.radius
-    except Exception:
-      print("You tried to compare an object without the .radius property")
-      return False
+  
+  def __str__(self) -> str:
+    return f'Circle class that is a child of Geometry_common class'
+  def __repr__(self): 
+    return f"Circle(x = {self._x}, y = {self._y}, radius = {self.radius})"
+
+
 
 ##################
 class Rectangle(Geometry_common):
+  """A class for creating rectangle objects."""
   def __init__(self, x, y, side1, side2):
+    """Initialize a Rectangle object with x,y coordinates and side1, side2 of the Rectangle"""
     super().__init__(x,y)
     self.side1 = side1
     self.side2 = side2
 
   def set_sides(self,side1,side2):
+    """Sets the side of the rectangle"""
     self.side1 = side1
     self.side2 = side2
-    
+  
   #Area / Circumfrance for Rectangle
-  def get_area(self):
-    area = self.side1 * self.side2
-    print(f"Area = {area}")
-    return area
-  def get_circumfrence(self):
-    circumfrance = self.side1*2 + self.side2*2
-    print(f"Circumfrance = {circumfrance}")
-    return circumfrance
+  @property
+  def area(self):
+    """Area property for the Rectangle Class"""
+    return self.side1 * self.side2
+  @property
+  def circumfrance(self):
+    """Circumfrance property for the Rectangle Class"""
+    return self.side1*2 + self.side2*2
   
   def is_square(self):
+    """Checks if the object is a square. Returns True if side1 == side2 otherwise returns False"""
     return self.side1 == self.side2
-    
+  
+  def is_inside(self, x, y): #Used chatGPT 3 to get this function
+    """Check if a point is inside the rectangle.
+
+        Args:
+            x (float or int): The x-coordinate of the point.
+            y (float or int): The y-coordinate of the point.
+
+        Returns:
+            bool: True if the point is inside the rectangle; False otherwise.
+
+        Example:
+            rect = Rectangle(x=0, y=0, side1=4, side2=5)
+            result = rect.is_inside(2, 2)
+            # result should be True
+        """
+    left_boundary = self._x - self.side1 / 2
+    right_boundary = self._x + self.side1 / 2
+    top_boundary = self._y + self.side2 / 2
+    bottom_boundary = self._y - self.side2 / 2
+    return left_boundary <= x <= right_boundary and bottom_boundary <= y <= top_boundary
+  
   def __str__(self) -> str:
-    return 'Rektangle class'
-  def __repr__(self) -> str:
-    
-    pass
+    return f'Rectangle class that is a child of Geometry_common class'
+  def __repr__(self):
+    return f"Rectangle(x = {self._x}, y = {self._y}, side1 = {self.side1}, side2 = {self.side2})"
